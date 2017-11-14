9c2816888d4ad2c32a957e6f7b35872515b60217
Stefan Bourlon <stefan.bourlon@ca.com>
2017-11-14 10:54:41 -0800

    Add basic authentication with ElasticSearch

--- Grafana/elasticsearch2elastic.py
+++ Grafana/elasticsearch2elastic.py
@@ -9,6 +9,7 @@ import sys
 
 # ElasticSearch Cluster to Monitor
 elasticServer = os.environ.get('ES_METRICS_CLUSTER_URL', 'http://server1:9200')
+elasticServerAuth = os.environ.get('ES_METRICS_CLUSTER_AUTH', None)
 interval = int(os.environ.get('ES_METRICS_INTERVAL', '60'))
 
 # ElasticSearch Cluster to Send Metrics
@@ -16,12 +17,12 @@ elasticIndex = os.environ.get('ES_METRICS_INDEX_NAME', 'elasticsearch_metrics')
 elasticMonitoringCluster = os.environ.get('ES_METRICS_MONITORING_CLUSTER_URL', 'http://server2:9200')
 
 
-def fetch_clusterhealth():
+def fetch_clusterhealth(auth=None):
     try:
         utc_datetime = datetime.datetime.utcnow()
         endpoint = "/_cluster/health"
         urlData = elasticServer + endpoint
-        response = urllib.urlopen(urlData)
+        response = http_client(urlData, auth)
         jsonData = json.loads(response.read())
         clusterName = jsonData['cluster_name']
         jsonData['@timestamp'] = str(utc_datetime.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3])
@@ -39,26 +40,27 @@ def fetch_clusterhealth():
         return clusterName
 
 
-def fetch_clusterstats():
+def fetch_clusterstats(auth=None):
     utc_datetime = datetime.datetime.utcnow()
     endpoint = "/_cluster/stats"
     urlData = elasticServer + endpoint
-    response = urllib.urlopen(urlData)
+    response = http_client(urlData, auth)
     jsonData = json.loads(response.read())
     jsonData['@timestamp'] = str(utc_datetime.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3])
     post_data(jsonData)
 
 
-def fetch_nodestats(clusterName):
+def fetch_nodestats(clusterName, auth=None):
     utc_datetime = datetime.datetime.utcnow()
     endpoint = "/_cat/nodes?v&h=n"
     urlData = elasticServer + endpoint
-    response = urllib.urlopen(urlData)
+    response = http_client(urlData, auth)
+
     nodes = response.read()[1:-1].strip().split('\n')
     for node in nodes:
         endpoint = "/_nodes/%s/stats" % node.rstrip()
         urlData = elasticServer + endpoint
-        response = urllib.urlopen(urlData)
+        response = http_client(urlData, auth)
         jsonData = json.loads(response.read())
         nodeID = jsonData['nodes'].keys()
         try:
@@ -70,18 +72,19 @@ def fetch_nodestats(clusterName):
             continue
 
 
-def fetch_indexstats(clusterName):
+def fetch_indexstats(clusterName, auth=None):
     utc_datetime = datetime.datetime.utcnow()
     endpoint = "/_stats"
     urlData = elasticServer + endpoint
-    response = urllib.urlopen(urlData)
+    response = http_client(urlData, auth)
+
     jsonData = json.loads(response.read())
     jsonData['_all']['@timestamp'] = str(utc_datetime.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3])
     jsonData['_all']['cluster_name'] = clusterName
     post_data(jsonData['_all'])
 
 
-def post_data(data):
+def post_data(data, auth=None):
     utc_datetime = datetime.datetime.utcnow()
     url_parameters = {'cluster': elasticMonitoringCluster, 'index': elasticIndex,
         'index_period': utc_datetime.strftime("%Y.%m.%d"), }
@@ -89,17 +92,37 @@ def post_data(data):
     headers = {'content-type': 'application/json'}
     try:
         req = urllib2.Request(url, headers=headers, data=json.dumps(data))
+
+        # Add Basic Authentication
+        if auth is not None:
+            username, password = auth.split(":")
+            basicAuthToken = base64.b64encode('%s:%s' % (username, password))
+            req.add_header("Authorization", "Basic %s" % basicAuthToken)
+
         f = urllib2.urlopen(req)
     except Exception as e:
         print "Error:  {}".format(str(e))
 
+def http_client(url, auth=None):
+    try:
+        req = urllib2.Request(url)
+
+        # Add Basic Authentication
+        if auth is not None:
+            username, password = auth.split(":")
+            basicAuthToken = base64.b64encode('%s:%s' % (username, password))
+            req.add_header("Authorization", "Basic %s" % basicAuthToken)
+
+        return urllib2.urlopen(req)
+    except Exception as e:
+        print "Error:  {}".format(str(e))
 
 def main():
-    clusterName = fetch_clusterhealth()
+    clusterName = fetch_clusterhealth(auth=elasticServerAuth)
     if clusterName != "unknown":
-        fetch_clusterstats()
-        fetch_nodestats(clusterName)
-        fetch_indexstats(clusterName)
+        fetch_clusterstats(auth=elasticServerAuth)
+        fetch_nodestats(clusterName, auth=elasticServerAuth)
+        fetch_indexstats(clusterName, auth=elasticServerAuth)
 
 
 if __name__ == '__main__':
